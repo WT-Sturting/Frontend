66f60bcf43f83e2e79506c66c7bec87f133a24b7
박준영 <pjy93525@gmail.com>
2022-08-25 17:42:06 +0900

    Revert "Revert "Revert "Revert "Merge pull request #4 from WT-Sturting/feature-jy-router""""
    This reverts commit c65af7d52bfc834d3299f56b86a7d4d8c8590424.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles/global.css";
+import { Routes, Route } from "react-router-dom";
+import Join from "./pages/join";
 
 function App() {
   return (
-    <>
-      <div>Sturting Project</div>
-    </>
+    <Routes>
+      <Route path='/join' element={<Join />}></Route>
+      <Route path='/login' element={<div>로그인</div>}></Route>
+      <Route path='/' element={<div>메인페이지</div>}></Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/join/index.tsx
@@ -0,0 +1,5 @@
+const Join = () => {
+  return <div>hi</div>;
+};
+
+export default Join;
