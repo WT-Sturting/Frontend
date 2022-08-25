4a3987fb4b3ea5e55170b67b098b25990dc16dea
LOCA <98865366+LOCA525@users.noreply.github.com>
2022-08-25 17:29:04 +0900

    Revert "라우터 설정 완료"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./styles/global.css";
-import { Routes, Route } from "react-router-dom";
-import Join from "./pages/join";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/join' element={<Join />}></Route>
-      <Route path='/login' element={<div>로그인</div>}></Route>
-      <Route path='/' element={<div>메인페이지</div>}></Route>
-    </Routes>
+    <>
+      <div>Sturting Project</div>
+    </>
   );
 }
 
