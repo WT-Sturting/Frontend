d3dfc9387403308e6153622210a5513c54e26fc8
박준영 <pjy93525@gmail.com>
2022-08-25 17:55:19 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path='/join' element={<Join />}></Route>
-      <Route path='/login' element={<div>로그인</div>}></Route>
+      <Route path='/login' element={<div>.로그인</div>}></Route>
       <Route path='/' element={<div>메인페이지</div>}></Route>
     </Routes>
   );
