67590e11e9da5003f9e28f67398a0c0b7f33d71d
박준영 <pjy93525@gmail.com>
2022-08-29 17:50:48 +0900

    mainPage 마크업 완료

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+
 const Map = () => {
-  return <div>지도</div>;
+  const { kakao } = window as any;
+
+  useEffect(() => {
+    const container = document.getElementById("myMap");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+    console.log(map);
+  }, []);
+
+  return (
+    <div
+      id='myMap'
+      style={{
+        width: "100%",
+        height: "calc(100vh-56px)",
+      }}
+    ></div>
+  );
 };
 
 export default Map;
