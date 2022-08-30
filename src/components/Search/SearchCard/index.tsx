f602c66fc606c5980ebe347424187dd1e1403a25
박준영 <pjy93525@gmail.com>
2022-08-30 17:47:20 +0900

    헤더 로고 포함 퍼블리싱 완료

--- src/components/Search/SearchCard/index.tsx
+++ src/components/Search/SearchCard/index.tsx
@@ -3,18 +3,36 @@ import styled from "styled-components";
 const SearchCard = () => {
   return (
     <SearchCardWrap>
-      <h1>스터디 이름</h1>
-      <p>스터디 카테고리</p>
-      <p>스터디 위치</p>
-      <p>스터디 인원</p>
+      <div className='picture'>사진 or 이미지</div>
+      <div className='cardContent'>
+        <h1>스터디 이름</h1>
+        <p>스터디 카테고리</p>
+        <p>스터디 위치</p>
+        <p>스터디 인원</p>
+      </div>
     </SearchCardWrap>
   );
 };
 
 const SearchCardWrap = styled.li`
+  display: flex;
   width: 100%;
   height: 200px;
   border-bottom: 1px solid rgb(231, 231, 231);
+  padding: 15px;
+
+  .picture {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 140px;
+    height: 170px;
+    border: 1px solid rgb(231, 231, 231);
+  }
+
+  .cardContent {
+    padding-left: 20px;
+  }
 `;
 
 export default SearchCard;
