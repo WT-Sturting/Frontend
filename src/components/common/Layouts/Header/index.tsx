df25594e82bfe7904e5095492713a1d8cd8e3371
박준영 <pjy93525@gmail.com>
2022-09-01 21:53:10 +0900

    참여자 권한 마크업완료 방장권한 마크업 진행중

--- src/components/common/Layouts/Header/index.tsx
+++ src/components/common/Layouts/Header/index.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className='menuBtn'>
         <button
           onClick={() => {
-            navigate("/");
+            navigate("/main");
           }}
         >
           지도
